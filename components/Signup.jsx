d6581592f9ed947c67ca93b05c3f78db2ba16bed
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-08-15 14:47:04 +0530

    Update Signup.jsx

--- components/Signup.jsx
+++ components/Signup.jsx
@@ -5,7 +5,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
-const SignIn = styled.div`
+
+const SignIn = styled.div
   max-width: 800px;
   width: 100%;
   background-color: #0e58ae;
@@ -16,9 +17,9 @@ const SignIn = styled.div`
   p {
     padding-top: 1rem;
   }
-  h2 {
+    h2{
     font-weight: 500;
-  }
+    }
 
   a {
     opacity: 0.8;
@@ -76,22 +77,21 @@ const SignIn = styled.div`
       }
     }
   }
-
-  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    @media (max-width: ${({ theme }) => theme.media.mobile}){
     width: 90% !important;
     height: 110vh !important;
-  }
-`;
+    }
+;
 
 const CustomToastContainer = styled(ToastContainer).attrs({
   // Custom class to apply styles
   toastClassName: 'custom-toast',
-})`
+})
   .custom-toast {
-    top: 60px !important;
+    top: 60px !important; 
     font-size: 1.25rem;
   }
-`;
+;
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
